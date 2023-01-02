be96ca0a402ba7f9598da6858d3635e1e96d070f
Ryan DeLong <rdelong111@gmail.com>
2023-01-01 18:26:46 -0600

    commit tree b71ef260baa729682210be18ddcb22b93a91fcc9

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -9,6 +9,18 @@ class LinkedList
   end
 
   def reverse!
-    # your code here
+    return self if @head.nil? || @head.next_node.nil?
+    
+    node = @head
+    @head = @tail
+    @tail = node
+
+    until node.nil?
+      prev = node.next_node
+      node.next_node = node.prev_node
+      node.prev_node = prev
+      node = prev
+    end
+    self
   end
 end
